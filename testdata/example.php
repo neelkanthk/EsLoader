38236115b10f8323a66c81f02fd287277a8cd598
neelkanthk <neelkanthkaushik2014@gmail.com>
2019-08-06 01:24:11 +0530

    Renamed bootstrap.php to example.php

--- testdata/example.php
+++ testdata/example.php
@@ -2,11 +2,11 @@
 
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-//Load classes
-require __DIR__ . '/vendor/autoload.php';
+
+require __DIR__ . '/../vendor/autoload.php';
 
 use Neelkanthk\EsLoader\Core\EsLoader;
 
 $app = new EsLoader();
 $fileName = 'data.json';
-$app->load(__DIR__ . "/testdata/$fileName");
+$app->load(__DIR__ . "/$fileName");
